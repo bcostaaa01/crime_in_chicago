618a282cbfb5d30c8bad68e4a7101d5b5353b538
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-03-05 18:44:28 +0000

    Loop over the crime_data list with a for loop

--- chicago_crime.py
+++ chicago_crime.py
@@ -26,3 +26,11 @@ print(crime_data[:10])
 # Create a Counter Object: crimes_by_month
 crimes_by_month = Counter()
 
+# Loop over the crime_data list
+for row in crime_data:
+    
+    # Convert the first element of each item into a Python Datetime Object
+    date = datetime.strptime(row[0], '%m/%d/%Y %I:%M:%S %p')
+    
+    # Increment the counter for the month of the row by one
+    crimes_by_month[date.month] += 1
